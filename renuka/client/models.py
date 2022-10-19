2a3c456ff843b59c7d25f3386c101d489284a9f5
unknown <bprajeeth285@gmail.com>
2022-10-19 15:18:37 +0530

    2nd commit-boutique

--- renuka/client/models.py
+++ renuka/client/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class BookUser(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    mobile = models.CharField(max_length=100, blank=False)
+    dress_type = models.CharField(max_length=20, blank=False) #dress type
+    #measurements
+    shoulder_to_shoulder = models.CharField(max_length=20, blank=False)
+    above_bust = models.CharField(max_length=20, blank=False)
+    top_bust = models.CharField(max_length=20, blank=False)
+    below_bust = models.CharField(max_length=20, blank=False)
+    blouse_length = models.CharField(max_length=20, blank=False)
+    arm_hole = models.CharField(max_length=20, blank=False)
+    arm_width = models.CharField(max_length=20, blank=False)
+    arm_length = models.CharField(max_length=20, blank=False)
+    waist = models.CharField(max_length=20, blank=False)
+    saree_length = models.CharField(max_length=20, blank=False)
+    saree_waist = models.CharField(max_length=20, blank=False)
+    knee_to_knee = models.CharField(max_length=20, blank=False)
+    thavani = models.CharField(max_length=20, blank=False)
+    pyjama_waist = models.CharField(max_length=20, blank=False)
+    pyjama_hip = models.CharField(max_length=20, blank=False)
+    pyjama_length = models.CharField(max_length=20, blank=False)
+    pyjama_ankle = models.CharField(max_length=20, blank=False)
+
--- renuka/client/views.py
+++ renuka/client/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse , HttpResponseRedirect
+from django.shortcuts import render
 # Create your views here.
-def home_page(request):
+def home_view(request):
+
+
+def form_view(request):
+    if request.method == 'POST':
+# VALUE INSIDE BRACKETS ARE ID FROM HTML TAGS
+        user_email = request.POST['mail_id']
+        username = request.POST['name']
+        dept = request.POST['dept']
+        type = request.POST['type']
+        try:
+            user = BookUser(isbn=book_isbn, email_id=user_email, name=username, dept=dept, type=type)
+            book_save = Book.objects.get(isbn=book_isbn)
+            book_save.books_count-=1
+            book_save.save()
+            user.save()         
+
+            return HttpResponseRedirect("/success")
+        except:
+            return HttpResponseRedirect("/fail")
+    else:
+        return render(request, "reserve.html")     
+
+def costumes_view(request):
+
+def ornaments_view(request):
+
+def accessories_view(request):
+
+
+
+
+
+
     
